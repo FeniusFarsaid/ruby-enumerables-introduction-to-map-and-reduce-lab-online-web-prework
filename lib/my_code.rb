e1601db18cae11ef6773f02a55cdce4d7af84853
David Ingram <david@ingramaviation.com>
2019-07-23 03:40:17 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,34 @@
 # My Code here....
+def map_to_negativize(source_array)
+  source_array.map{|n| n * -1}
+  
+end
+
+def map_to_no_change(dune)
+  dune.each{}
+  
+end 
+
+def map_to_double(source_array)
+  source_array.map{|n| n * 2}
+  
+end
+
+def map_to_square(source_array)
+  source_array.map{|n| n ** 2}
+  
+end 
+
+#def reduce_to_total(source_array, starting_point = 100)
+  #if 
+  #source_array.sum 
+  
+#end 
+
+def reduce_to_total(source_array, starting_point = 0)
+  
+  source_array.sum + starting_point
+  
+end 
+
+def reduce_to_all_true 
